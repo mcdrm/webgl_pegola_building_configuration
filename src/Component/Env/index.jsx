2dd545bf8c520a0950aca6a22c6263ccf9ad267b
SMART <mario.captain7105@gmail.com>
2024-11-18 17:31:07 +0200

    fix: env map

--- src/Component/Env/index.jsx
+++ src/Component/Env/index.jsx
@@ -17,7 +17,7 @@ const Env = () => {
                 background
                 backgroundIntensity={5}
                 backgroundBlurriness={isShowBg ? 0 : 1}
-                ground={isShowBg ? { height: 20, radius: 100, scale: 20 } : null}
+                ground={isShowBg ? { height: 30, radius: 350, scale: 25 } : null}
             />
             <OrbitControls
                 target={[0, height / 3, 0]}
